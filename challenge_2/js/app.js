30f79931d06d472ce0af953b44c8e14165dd5517
Brianna Veenstra <b.g.veenstra@gmail.com>
2017-01-04 12:14:52 -0800

    improve challenge 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -2,7 +2,7 @@ console.log('challenge #2 -- app.js loaded!');
 
 // var app = angular.module("appTwo", []);
 // app.controller("WelcomeController", WelcomeController);
-
+//
 // function WelcomeController(){
 //   this.full_name = "YOUR NAME";
 // }
